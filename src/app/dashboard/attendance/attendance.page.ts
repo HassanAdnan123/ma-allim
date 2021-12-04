b48588e60ded7858158efd6df165a7b78bad37fd
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2021-12-04 22:28:25 +0500

    Improvements in CSS of Grid

--- src/app/dashboard/attendance/attendance.page.ts
+++ src/app/dashboard/attendance/attendance.page.ts
@@ -11,16 +11,18 @@ export class AttendancePage implements OnInit {
   APIdata: any
   constructor(private dataService: DataService) { }
   data = []
+  recordsQty = ""
   header = {"period": "Period",  "logindate": "Login Date",  "name": "Name",  "logintime": "Login Time"}
   ngOnInit() {
   }
   async callService(){
-     this.APIdata = await this.dataService.sendPostRequest(7)
+     this.APIdata = await this.dataService.sendPostRequest(parseInt(this.recordsQty))
     console.log(this.APIdata)
     this.populateGrid(this.APIdata)
   }
   populateGrid(datagrid: any)
   {
+    this.data = []
     datagrid.forEach(element => {
       this.data.push({period: element.period, logindate: element.logindate, name: element.name, logintime: element.logintime})
     });

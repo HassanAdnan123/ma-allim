923531ac9735e70e6e6538bf3b79a15e5de6636a
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2021-12-05 14:55:32 +0500

    Loader added
    Loader added on backend error in attendance component

--- src/app/dashboard/attendance/attendance.page.ts
+++ src/app/dashboard/attendance/attendance.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './../../services/data.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-attendance',
@@ -9,24 +10,59 @@ import { DataService } from './../../services/data.service';
 export class AttendancePage implements OnInit {
 
   APIdata: any
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private alertController: AlertController) { }
   data = []
-  recordsQty = ""
-  header = {"period": "Period",  "logindate": "Login Date",  "name": "Name",  "logintime": "Login Time"}
+  startloader = false
+    userid = ""
+    month = ""
+    year = "2021"
+
+  header = {"period": "Period",  
+            "logindate": "Login Date",  
+            "name": "Name",
+            "logintime": "Login Time",
+            "status":"Status",
+            "minuteslate": "Minutes Late"
+          }
   ngOnInit() {
   }
   async callService(){
-     this.APIdata = await this.dataService.sendPostRequest(parseInt(this.recordsQty))
-    console.log(this.APIdata)
-    this.populateGrid(this.APIdata)
+    this.startloader = true
+     this.APIdata = await this.dataService.sendPostRequest({userid: this.userid, month: this.month, year: this.year})
+    
+     if(!this.APIdata.error)
+     this.populateGrid(this.APIdata)
+     else await this.presentAlert("Server down","Unable to get data from database server!");
+     this.startloader = false
   }
   populateGrid(datagrid: any)
   {
     this.data = []
     datagrid.forEach(element => {
-      this.data.push({period: element.period, logindate: element.logindate, name: element.name, logintime: element.logintime})
+      this.data.push({period: element.period, 
+                      logindate: element.datelogged, 
+                      name: element.name, 
+                      logintime: element.logintime,
+                      minuteslate: element.minuteslate,
+                      status: element.status
+                    })
     });
 
+  }
+
+  async presentAlert(errortitle: string, errormessage: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: errortitle,
+      //subHeader: errortitle,
+      message: errormessage,
+      buttons: ['Dismiss']
+    });
+
+    await alert.present();
+
+    //const { role } = await alert.onDidDismiss();
+    //console.log('onDidDismiss resolved with role', role);
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,9 +11,10 @@ export class DataService {
   constructor(public httpClient: HttpClient) { }
 
   responseFromAPI: any
+  setNoResponse = false
 
-  async sendPostRequest(noOfRecs) {
-
+  async sendPostRequest(requestJson) {
+    this.setNoResponse = false
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -22,16 +23,22 @@ export class DataService {
     }
 
     let postRequest = {
-      "noOfRecs": noOfRecs
+      "userid" : requestJson.userid,
+      "month" : requestJson.month,
+      "year": requestJson.year
     }
 
     await this.httpClient.post("https://localhost:44385/api/maallim", postRequest, httpOptions).toPromise().then(data => {
       this.responseFromAPI = data
     }, error => {
-      console.log(error);
+      this.setNoResponse = true
     })
-
-    return this.responseFromAPI
+    if(!this.setNoResponse)
+      return this.responseFromAPI
+    else { 
+      console.log("Error response is set to: "+this.setNoResponse)
+      return {error: this.setNoResponse} 
+    }
   }
 
 }

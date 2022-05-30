06ae43bead279cdd175cfea6318e12b9e40e930f
Hassan Adnan <43107637+HassanAdnan123@users.noreply.github.com>
2022-05-30 23:52:45 +0500

    heroku deployment

--- src/app/dashboard/departments/departments.module.ts
+++ src/app/dashboard/departments/departments.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,7 @@ import { DepartmentsPage } from './departments.page';
     IonicModule,
     DepartmentsPageRoutingModule
   ],
-  declarations: [DepartmentsPage]
+  declarations: [DepartmentsPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DepartmentsPageModule {}
